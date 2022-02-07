bcd47de4b7c08270c11963dccc54620ed9788f49
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-02-07 16:44:18 -0300

    Obtener dominios con su ip pública

--- app.js
+++ app.js
@@ -14,9 +14,28 @@ const obtenerDominios = async () => {
 
     const data = await response.json();
 
-    for(let i = 0; i < data.result.length; i++){
-        console.log('Dominio: ', data.result[i].name);
+    for (let i = 0; i < data.result.length; i++) {
+        obtenerIdZona(data.result[i].name, data.result[i].id);
     }
+
+}
+
+const obtenerIdZona = async (dominio, iddominio) => {
+    const responseZona = await fetch(`https://api.cloudflare.com/client/v4/zones/${iddominio}/dns_records`, {
+        method: 'get',
+        headers: {
+            'x-auth-key': process.env.XAUTHKEY,
+            'x-auth-email': process.env.XAUTHEMAIL
+        }
+    });
+
+    const idZonas = await responseZona.json();
+    console.log('Dominio: ', dominio);
+    console.log('Id dominio: ', iddominio);
+    console.log('id dns: ', idZonas.result[0].id);
+    console.log('ip pública: ', idZonas.result[0].content);
+    console.log();
 }
 
 obtenerDominios();
+
