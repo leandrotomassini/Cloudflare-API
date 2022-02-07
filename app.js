4daa7617befa12b1596034eb872f2460f863c46e
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-02-07 17:55:37 -0300

    Ip pública

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ require('dotenv').config();
 
 const fetch = require('node-fetch');
 
-const obtenerDominios = async () => {
+
+const obtenerMiIpPublica = () => {
+
+    const http = require('http');
+    let ipPublica;
+
+    ipPublica = http.get({ 'host': 'api.ipify.org', 'port': 80, 'path': '/' }, function (resp) {
+        resp.on('data', function (ip) {
+            ipPublica = `${ip}`;
+            obtenerDominios(ipPublica);
+        });
+    });
+}
+
+const obtenerDominios = async (ipPublica) => {
 
     const response = await fetch('https://api.cloudflare.com/client/v4/zones', {
         method: 'get',
@@ -15,12 +29,12 @@ const obtenerDominios = async () => {
     const data = await response.json();
 
     for (let i = 0; i < data.result.length; i++) {
-        obtenerIdZona(data.result[i].name, data.result[i].id);
+        obtenerIdZona(data.result[i].name, data.result[i].id, ipPublica);
     }
 
 }
 
-const obtenerIdZona = async (dominio, iddominio) => {
+const obtenerIdZona = async (dominio, iddominio, ipPublica) => {
     const responseZona = await fetch(`https://api.cloudflare.com/client/v4/zones/${iddominio}/dns_records`, {
         method: 'get',
         headers: {
@@ -34,8 +48,12 @@ const obtenerIdZona = async (dominio, iddominio) => {
     console.log('Id dominio: ', iddominio);
     console.log('id dns: ', idZonas.result[0].id);
     console.log('ip pública: ', idZonas.result[0].content);
+    console.log('Mi ip: ', ipPublica);
     console.log();
 }
 
-obtenerDominios();
 
+
+
+
+obtenerMiIpPublica();
